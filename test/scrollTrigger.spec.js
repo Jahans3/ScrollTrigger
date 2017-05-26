d1e976a600a3a382244b34da4fae7add886e88c0
Admin <admin@jamess-laptop.redant.local>
2017-05-26 14:33:11 +0100

    Add extensive unit test suite

---
+++ test/scrollTrigger.spec.js
@@ -0,0 +1,6 @@
+const mocha = require('mocha')
+const assert = require('assert')
+
+mocha.describe('ScrollTrigger', function () {
+  mocha.it('should be the best DOM extension in the world', done => done())
+})
